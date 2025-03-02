0524b35626b1d5b449179965ceb0ed034d47de6e
Dawit <shawerthaughts@proton.me>
2025-03-02 18:50:49 +0300

    fix: changed react vesion for stability, complete: navigation section

--- src/App.jsx
+++ src/App.jsx
@@ -8,32 +8,33 @@ import {
   Subscribe,
   SuperQuality,
 } from "./sections";
+import Nav from "./components/Nav.jsx";
 
 const app = () => (
   <main className="relative">
-    {/*<Nav />*/}
-    <section className={"xl:padding-1 wide:padding-r padding-b"}>
+    <Nav />
+    <section className="xl:padding-1 wide:padding-r padding-b">
       <Hero />
     </section>
-    <section className={"padding"}>
+    <section className="padding">
       <PopularProducts />
     </section>
-    <section className={"padding"}>
+    <section className="padding">
       <SuperQuality />
     </section>
-    <section className={"padding-x py-10"}>
+    <section className="padding-x py-10">
       <Services />
     </section>
-    <section className={"padding"}>
+    <section className="padding">
       <SpecialOffer />
     </section>
-    <section className={"bg-pale-blue padding"}>
+    <section className="bg-pale-blue padding">
       <CustomerReviews />
     </section>
-    <section className={"padding-x py-16 w-full sm:py-32"}>
+    <section className="padding-x py-16 w-full sm:py-32">
       <Subscribe />
     </section>
-    <section className={"pb-8 bg-black padding-x padding-t"}>
+    <section className="pb-8 bg-black padding-x padding-t">
       <Footer />
     </section>
   </main>
