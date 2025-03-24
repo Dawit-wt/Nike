8d1b6c12c460705232296cbf350db10f01b07a50
Dawit <shawerthaughts@proton.me>
2025-03-24 17:47:59 +0300

    completed hero section, and fixed layout for navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,43 @@
 import {
-  CustomerReviews,
-  Footer,
-  Hero,
-  PopularProducts,
-  Services,
-  SpecialOffer,
-  Subscribe,
-  SuperQuality,
+    CustomerReviews,
+    Footer,
+    Hero,
+    PopularProducts,
+    Services,
+    SpecialOffer,
+    Subscribe,
+    SuperQuality,
 } from "./sections";
 import Nav from "./components/Nav.jsx";
 
 const app = () => (
-  <main className="relative">
-    <Nav />
-    <section className="xl:padding-1 wide:padding-r padding-b">
-      <Hero />
-    </section>
-    <section className="padding">
-      <PopularProducts />
-    </section>
-    <section className="padding">
-      <SuperQuality />
-    </section>
-    <section className="padding-x py-10">
-      <Services />
-    </section>
-    <section className="padding">
-      <SpecialOffer />
-    </section>
-    <section className="bg-pale-blue padding">
-      <CustomerReviews />
-    </section>
-    <section className="padding-x py-16 w-full sm:py-32">
-      <Subscribe />
-    </section>
-    <section className="pb-8 bg-black padding-x padding-t">
-      <Footer />
-    </section>
-  </main>
+    <main className="relative">
+        <Nav/>
+        <section className="xl:padding-l wide:padding-r padding-b">
+            <Hero/>
+        </section>
+        <section className="padding">
+            <PopularProducts/>
+        </section>
+        <section className="padding">
+            <SuperQuality/>
+        </section>
+        <section className="padding-x py-10">
+            <Services/>
+        </section>
+        <section className="padding">
+            <SpecialOffer/>
+        </section>
+        <section className="bg-pale-blue padding">
+            <CustomerReviews/>
+        </section>
+        <section className="padding-x py-16 w-full sm:py-32">
+            <Subscribe/>
+        </section>
+        <section className="pb-8 bg-black padding-x padding-t">
+            <Footer/>
+        </section>
+    </main>
 );
 
 export default app;
