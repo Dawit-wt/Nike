c47472046065b2b6dd209b6b184e2d8fd0866182
Dawit <shawerthaughts@proton.me>
2025-03-26 13:55:50 +0300

    completed the footer section,and fixed input field bug

--- src/sections/Subscribe.jsx
+++ src/sections/Subscribe.jsx
@@ -2,17 +2,14 @@ import Button from "../components/Button.jsx";
 
 const Subscribe = () => {
     return (
-        <section
-            id="contact-us"
-            className="max-container"
-        >
+        <section id="contact-us" className="max-container">
             <div className="flex flex-col flex-wrap lg:flex-row lg:justify-between">
                 <h3 className="font-palanquin text-4xl leading-[68px] capitalize font-bold lg:max-w-md text-center">
                     Sign Up for <span className="text-coral-red">Updates</span> &
                     Newsletter
                 </h3>
                 <div
-                    className="lg:max-w-full md:flex-row flex items-center flex-col gap-5 p-2.5 sm:border sm:border-slate-gray rounded-full mt-6">
+                    className="w-full flex flex-col sm:flex-row sm:justify-between items-center gap-3 p-2.5 sm:border sm:border-gray-300 sm:rounded-full mt-6 lg:max-w-[40%] lg:max-h-[78px]">
                     <input
                         type="text"
                         className="input"
